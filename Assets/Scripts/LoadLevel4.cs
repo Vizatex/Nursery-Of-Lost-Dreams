92c77cbe7e32e583168fab3163d45f4e1520e33c
Vizatex <vizatex@nauruconsulting.com>
2024-12-17 21:59:34 +0100

    New Version

---
+++ Assets/Scripts/LoadLevel4.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadLevel4 : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
